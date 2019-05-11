5445c93c7b66122558d350239dcb278785e0aee2
saiyerniakhil <saiyerniakhil>
2019-05-11 17:42:53 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let email = {
+    sender : 'React Newsletter',
+    subject: 'React Newsletter - issue 36',
+    date : 'Jul 15',
+    message: '*React Newsletter - issue 36 Read this issue on the web http://www.reactjs.org or just click the link in the mail'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Mail({ emailBody }) {
+    return(
+        <div>
+        <div className='total-email'>
+        <div className='check-box'>
+        <input type='checkbox' name='mail-select'/>
+        </div>
+        <div>
+        <label htmlFor="mail-select">
+            <div className='single-line-email'>
+                <div className='first-line'>
+                    <Sender sender={emailBody.sender}/>
+                    <Subject sub={emailBody.subject}/>
+                    <Date dateRec={emailBody.date}/>
+                </div>
+                <div className='msg-glimpse'>
+                    <Message msg={emailBody.message}/>
+                </div> 
+            </div>
+        </label>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+let Message = ({msg}) => {
+    return(
+        <div>
+            {msg}
+        </div>
+    )
+}
+
+let Date = ({dateRec}) => {
+    return(
+        <div>
+            {dateRec}
+        </div>
+    )
+}
+
+
+let Subject = ({sub}) => {
+    return(
+        <div>
+            {sub}
+        </div>
+    )
+}
+
+let Sender = ({sender}) => {
+    return(
+        <div>
+            {sender}
+        </div>
+    )
+}
+
+ReactDOM.render(<Mail emailBody={email}/>,document.querySelector('#root'))
+
